41f8403ec498a8c2447017dd24a55aaef3c75969
Tito Nobre <titonobre@gmail.com>
2019-05-07 02:06:49 +0100

    upgrade to storybook 5

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,10 +1,15 @@
 const path = require("path");
 
-module.exports = (storybookBaseConfig, configType) => {
-  storybookBaseConfig.resolve.alias = {
-    ...storybookBaseConfig.resolve.alias,
-    angular: require.resolve("angular")
-  };
+module.exports = ({ config }) => {
+  return {
+    ...config,
+    resolve: {
+      ...config.resolve,
+      alias: {
+        ...(config.resolve && config.resolve.alias),
+        angular: require.resolve("angular")
 
-  return storybookBaseConfig;
+      }
+    }
+  };
 };
