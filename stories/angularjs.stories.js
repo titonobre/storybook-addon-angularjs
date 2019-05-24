07527803d731cfeae89ab8c79f09ddce55301158
titonobre <titonobre@users.noreply.github.com>
2019-05-24 10:50:21 +0100

    update the story with an evaluated binding

--- stories/angularjs.stories.js
+++ stories/angularjs.stories.js
@@ -11,6 +11,7 @@ storiesOf("Demo Component", module)
     "default",
     forModule("myApp").createElement(compile => {
       const name = text("Name", "Jane");
+      const someString = text("Some String", "It works too!");
 
       const foo = {
         bar: number("Value", 20, { range: true, min: 0, max: 30, step: 1 })
@@ -20,6 +21,6 @@ storiesOf("Demo Component", module)
 
       const onEvt = action("clicked");
 
-      return compile`<demo-component name="${name}" foo="${foo}" things="${things}" on-event="${onEvt}(item)"></demo-component>`;
+      return compile`<demo-component name="${name}" some-string="{{${someString}}}" foo="${foo}" things="${things}" on-event="${onEvt}(item)"></demo-component>`;
     })
   );
