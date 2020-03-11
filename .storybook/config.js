162e32d8eddbd8f66d5a57cb19eb93b10026eb4a
Tito Nobre <tito.nobre@blip.pt>
2020-03-11 02:41:21 +0000

    update dependencies

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,9 +2,4 @@ import "./bootstrap";
 import { configure } from "@storybook/html";
 
 // automatically import all files ending in *.stories.js
-const req = require.context("../stories", true, /.stories.js$/);
-function loadStories() {
-  req.keys().forEach(filename => req(filename));
-}
-
-configure(loadStories, module);
+configure(require.context("../stories/", true, /\.stories\.js$/), module);
--- stories/angularjs.stories.js
+++ stories/angularjs.stories.js
@@ -1,26 +1,24 @@
-import { storiesOf } from "@storybook/html";
-
 import { withKnobs, text, number, array } from "@storybook/addon-knobs";
 import { action } from "@storybook/addon-actions";
 
 import { forModule } from "storybook-addon-angularjs";
 
-storiesOf("Demo Component", module)
-  .addDecorator(withKnobs)
-  .add(
-    "default",
-    forModule("myApp").createElement(compile => {
-      const name = text("Name", "Jane");
-      const someString = text("Some String", "It works too!");
+export default {
+  title: "Demo Component",
+  decorators: [withKnobs]
+};
 
-      const foo = {
-        bar: number("Value", 20, { range: true, min: 0, max: 30, step: 1 })
-      };
+export const Demo = forModule("myApp").createElement(compile => {
+  const name = text("Name", "Jane");
+  const someString = text("Some String", "It works too!");
 
-      const things = array("Things", ["a", "b", "c"], ",");
+  const foo = {
+    bar: number("Value", 20, { range: true, min: 0, max: 30, step: 1 })
+  };
 
-      const onEvt = action("clicked");
+  const things = array("Things", ["a", "b", "c"], ",");
 
-      return compile`<demo-component name="${name}" some-string="{{${someString}}}" foo="${foo}" things="${things}" on-event="${onEvt}(item)"></demo-component>`;
-    })
-  );
+  const onEvt = action("clicked");
+
+  return compile`<demo-component name="${name}" some-string="{{${someString}}}" foo="${foo}" things="${things}" on-event="${onEvt}(item)"></demo-component>`;
+});
