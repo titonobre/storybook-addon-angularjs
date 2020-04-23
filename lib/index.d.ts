2c4c072c2880c4e4b7d67c50671ffdb5e327801d
xemlock <952555+xemlock@users.noreply.github.com>
2020-04-23 09:47:09 +0200

    Add TypeScript type declarations

---
+++ lib/index.d.ts
@@ -0,0 +1,3 @@
+export declare function withAngularJs(module: string|string[]): any;
+export declare function html(strings: TemplateStringsArray, ...values: any): { template: string; props: any };
+export declare function template(strings: TemplateStringsArray, ...values: any): { template: string; props: any };
