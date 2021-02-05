7119f5186e943aa221d0b50ff01093ace6819f4e
Gonçalo Matos <goncalomatos3@gmail.com>
2021-02-05 22:42:54 +0000

    add support for web components

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,4 @@
 export declare function withAngularJs(module: string|string[]): any;
 export declare function html(strings: TemplateStringsArray, ...values: any): { template: string; props: any };
 export declare function template(strings: TemplateStringsArray, ...values: any): { template: string; props: any };
+export declare function wc(componentName: string, bindings: any): { template: string; props: any };
